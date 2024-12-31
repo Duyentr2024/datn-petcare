3d268812353ab4b4c8397cf2b53336d370c22c9b
LuuBaro <baolgpc08011@fpt.edu.vn>
2024-12-31 16:08:04 +0700

    #16 - LuuBaro - [theme] - component: Giá tốt mỗi ngày

--- petcare-fe/src/App.jsx
+++ petcare-fe/src/App.jsx
@@ -1,20 +1,25 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
-import Login from './components/account/Login'  // Đảm bảo bạn đã import component Login
-import Register from './components/account/Register'
-
+import { useState } from "react";
+import Banner from "./components/banner/banner.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./components/account/Login"; // Đảm bảo bạn đã import component Login
+import Register from "./components/account/Register";
+import GoodPrice from "./components/card/GoodPrice.jsx";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/good-price" element={<GoodPrice />} />
+        <Route path="/" element={<Banner />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
