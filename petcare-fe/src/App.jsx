4e186aa79e28eb5551e2e45e8c8aedb3ec12822e
DianNg25 <nguyenncpc07985@fpt.edu.vn>
2025-01-01 17:33:33 +0700

    #13 - DianNg25 - [theme] - component: Trang Sản Phẩm

--- petcare-fe/src/App.jsx
+++ petcare-fe/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import {useState} from "react";
 import Banner from "./components/banner/banner.jsx";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import "./App.css";
 import Login from "./components/account/Login"; // Đảm bảo bạn đã import component Login
 import Register from "./components/account/Register";
@@ -8,21 +8,22 @@ import GoodPrice from "./components/card/GoodPrice.jsx";
 import Contact from "./pages/Contact.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import ProductList from "./components/product/ProductPage.jsx";
+
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/good-price" element={<GoodPrice />} />
-        <Route path="/" element={<Banner />} />
-          <Route path="/contact" element={<Contact />} />
-
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/good-price" element={<GoodPrice/>}/>
+                <Route path="/" element={<Banner/>}/>
+                <Route path="/product-list" element={<ProductList/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
