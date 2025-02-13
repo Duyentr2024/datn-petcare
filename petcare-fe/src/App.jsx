2e361d61630be936de140930631400dc46a9e7a8
LuuBaro <baolgpc08011@fpt.edu.vn>
2025-02-13 09:32:13 +0700

    Cấu hình phần role xong

--- petcare-fe/src/App.jsx
+++ petcare-fe/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./page/HomePage.jsx";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx"; // Import AuthProvider
-import Admin from "./page/Admin.jsx"
-function App() {
-    const [count, setCount] = useState(0);
+import { AuthProvider } from "./context/AuthContext.jsx";
 
+function App() {
     return (
-      <AuthProvider> {/* Bọc toàn bộ ứng dụng trong AuthProvider */}
+      <AuthProvider>
         <Router>
           <Routes>
+            {/* Route chính cho Petcare */}
             <Route path="/*" element={<HomePage />} />
-            <Route path="/admin" element={<Admin />} />
+
+
+
           </Routes>
         </Router>
       </AuthProvider>
--- petcare-fe/src/page/HomePage.jsx
+++ petcare-fe/src/page/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/header/Header.jsx";
 import Banner from "../components/banner/banner.jsx";
-import RenderFooter from "../components/footer/Footer.jsx"
+import RenderFooter from "../components/footer/Footer.jsx";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import ItemPageHome from "./ItemPageHome.jsx";
 import ProductDetail from "../components/productDetail/ProductDetail.jsx";
@@ -13,41 +13,35 @@ import ShoppingCart from "../components/cart/ShoppingCart.jsx";
 import Login from "../components/account/Login.jsx";
 import Register from "../components/account/Register.jsx";
 import ForgotPassword from "../components/account/ForgotPassword.jsx";
-import MyAccount from "../components/account/MyAccount.jsx"
+import MyAccount from "../components/account/MyAccount.jsx";
 import VerifyOTP from "../components/account/VerifyOTP.jsx";
 import OrderOffline from "../components/orderOffline/OrderOffline.jsx";
-import Admin from "../page/Admin.jsx"
+import NotFoundPage from "./NotFoundPage.jsx";
 const HomePage = () => {
+  return (
+    <>
+      <Header />
 
-    return (
-        <>
-            <Header />
+      <Routes>
+        <Route path="/productDetail" element={<ProductDetail />} />
+        <Route path="/productPage" element={<ProductPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newsPage" element={<NewsPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/shoppingCart" element={<ShoppingCart />} />
+        <Route path="/" element={<ItemPageHome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/my-account/*" element={<MyAccount />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} />
+        <Route path="/orderOffline" element={<OrderOffline />} />
+        {/* Route 404 - Khi không tìm thấy trang */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
 
-            <Routes>
-                <Route path="/productDetail" element={<ProductDetail />} />
-                <Route path="/productPage" element={<ProductPage/>} />
-                <Route path="/contact" element={<Contact/>} />
-                <Route path="/newsPage" element={<NewsPage/>} />
-                <Route path="/checkout" element={<Checkout/>} />
-                <Route path="/shoppingCart" element={<ShoppingCart/>} />
-                <Route path="/" element={<ItemPageHome />} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/register" element={<Register/>} />
-                <Route path="/forgotPassword" element={<ForgotPassword/>} />
-                <Route path="/my-account/*" element={<MyAccount/>} />
-                <Route path="/verify-otp" element={<VerifyOTP/>} />
-                <Route path="/orderOffline" element={<OrderOffline/>} />
-                <Route path="/admin" element={<Admin/>} />
-      
-
-
-            </Routes>
-
-            <RenderFooter />
-
-            
-
-        </>
-    );
+      <RenderFooter />
+    </>
+  );
 };
 export default HomePage;
