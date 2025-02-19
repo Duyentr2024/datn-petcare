b3865cd0d9b8fe77f1a1f9db5881f661e3719fc7
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2025-02-19 21:40:09 +0700

    fix oder

--- petcare-fe/src/components/pay/Checkout.jsx
+++ petcare-fe/src/components/pay/Checkout.jsx
@@ -283,13 +283,13 @@ const Checkout = () => {
 
     const shippingAddress = `${selectedAddress.street}, ${selectedAddress.ward || ''}, ${selectedAddress.district || ''}, ${selectedAddress.province || ''}`.replace(/, ,/g, ',').replace(/, $/, '');
 
-
+    const shippingCosts = 30000 ;
     const handlePayment = async () => {
         const orderDetails = {
             userId: Number(userId),
             paymentMethod: String(paymentMethod),
             shippingAddress: String(shippingAddress),
-            shippingCost: Number(0),
+            shippingCost: Number(shippingCosts),
             voucherId: selectedAddress?.voucherId ? Number(selectedAddress.voucherId) : null,
             type: "ORDER ONLINE",
             items: products.map(({ productDetailId, quantityItem, price }) => ({
@@ -417,6 +417,7 @@ const Checkout = () => {
                                 </select>
                             </div>
 
+
                             <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
                                 <label className="block text-sm font-medium text-gray-700 md:w-1/3">
                                     Phường / Xã
@@ -436,6 +437,19 @@ const Checkout = () => {
                                     ))}
                                 </select>
                             </div>
+
+                            <div className="flex flex-col md:flex-row md:items-center md:space-x-4">
+                                <label className="block text-sm font-medium text-gray-700 md:w-1/3">
+                                    Giảm giá
+                                </label>
+                                <select
+                                    className="mt-1 block w-full md:w-2/3 border border-gray-300 rounded-md shadow-sm p-2 bg-white disabled:bg-gray-200">
+                                    <option value="">Chọn mã giảm giá</option>
+                                    <option value="10off">Giảm 10%</option>
+                                    <option value="20off">Giảm 20%</option>
+                                </select>
+
+                            </div>
                         </form>
 
                         {/* Nút Lưu Địa Chỉ */}
@@ -447,7 +461,6 @@ const Checkout = () => {
                                 </button>
                             </div>
                         )}
-
 
 
                         <div className="mt-8">
@@ -515,12 +528,12 @@ const Checkout = () => {
                             </div>
                             <div className="flex justify-between font-medium">
                                 <p>Phí giao hàng:</p>
-                                <p>0₫</p>
+                                <p>{shippingCosts}₫</p>
                             </div>
                             <div className="flex justify-between font-bold text-lg mt-2 border-t border-white pt-3">
                                 <p>Tổng cộng:</p>
                                 <p className="text-xl">{(
-                                    products.reduce((total, item) => total + item.price * item.quantityItem, 0)
+                                    products.reduce((total, item) => total + (item.price + shippingCosts)* item.quantityItem, 0)
                                 ).toLocaleString()}₫</p>
                             </div>
                         </div>
