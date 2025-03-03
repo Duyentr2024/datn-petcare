466d20faabdfeee2f7e3d5e0d38b4c83bb5e1347
Duyentr2024 <156353407+Duyentr2024@users.noreply.github.com>
2025-03-03 16:42:38 +0700

    appointment

--- petcare-fe/src/App.jsx
+++ petcare-fe/src/App.jsx
@@ -2,22 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./page/HomePage.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import Admin from "./page/Admin.jsx";
-import ManageProductColor from "./components/Manage/ManageProductColor";
-import ManageProductSize from "./components/Manage/ManageProductSize";
-import ManageProductWeights from "./components/Manage/ManageProductWeights";
-import ManageProductBrand from "./components/Manage/ManageProductBrand";
-import ManageProductCategories from "./components/Manage/ManageProductCategories";
-import ManageProducts from "./components/Manage/ManageProducts";
-import ManageProductImages from "./components/Manage/ManageProductImages";
 import TopButton from "./elements/TopButton";
 import SpaPage from "./page/SpaPage.jsx";
-import AppoinmentPage from "./page/AppointmentPage.jsx";
+import AppointmentPage from "./page/AppointmentPage.jsx";
 import ChatBot from "./components/ChatBot";
 import PrivateRoute from "./context/PrivateRoute.jsx";
 
-import AppointmentPage from "./page/AppointmentPage.jsx";
-import ChatBot from './components/ChatBot';
-import AdminSpa from "./page/AdminSpa.jsx";
+
 function App() {
   return (
     <AuthProvider>
@@ -27,7 +18,7 @@ function App() {
           <Route path="/*" element={<HomePage />} />
 
           <Route path="/spa" element={<SpaPage />} />
-          <Route path="/appointment" element={<AppoinmentPage />} />
+          <Route path="/appointment" element={<AppointmentPage />} />
 
           {/* Chỉ admin vào được */}
           <Route element={<PrivateRoute requiredRoles={["ADMIN", "STAFF"]} />}>
