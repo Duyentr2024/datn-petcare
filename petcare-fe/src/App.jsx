e5cc943a58b232d772a279d41d1bc3dbf89877aa
LuuBaro <baolgpc08011@fpt.edu.vn>
2025-03-05 22:13:37 +0700

    Commit CartCount Header & Notification

--- petcare-fe/src/App.jsx
+++ petcare-fe/src/App.jsx
@@ -7,7 +7,7 @@ import SpaPage from "./page/SpaPage.jsx";
 import AppointmentPage from "./page/AppointmentPage.jsx";
 import ChatBot from "./components/ChatBot";
 import PrivateRoute from "./context/PrivateRoute.jsx";
-import AdminSpa from "./page/AdminSpa.jsx";
+import AdminSpa from "./components/spaGrooming/admin/AdminAppointment.jsx";
 
 import OrderOffline from "./components/orderOffline/OrderOffline.jsx"
 
